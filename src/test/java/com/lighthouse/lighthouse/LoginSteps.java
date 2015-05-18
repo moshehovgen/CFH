a0eaf1418189de5617a9f80dbef290c7653b5b06
ronenPerion <ronen.yurik@perion.com>
2015-05-18 17:02:35 +0300

    adding saucelabs code in order to run test in the cloud

--- src/test/java/com/lighthouse/lighthouse/LoginSteps.java
+++ src/test/java/com/lighthouse/lighthouse/LoginSteps.java
@@ -1,17 +1,19 @@
 package com.lighthouse.lighthouse;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import cucumber.api.java.After;
-import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -22,10 +24,22 @@ import cucumber.api.java.en.When;
 		
 		
 		
-		@Before
+		/*@Before
 		public void initiateBrowser(){
 			dr = new FirefoxDriver();
-		}
+		}*/
+		
+		public void setUpLocal()  throws Throwable
+	    {
+	        //Setup for running Appium test in local environment
+			DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+			desiredCapabilities.setBrowserName(System.getenv("SELENIUM_BROWSER"));
+			desiredCapabilities.setVersion(System.getenv("SELENIUM_VERSION"));
+			desiredCapabilities.setCapability(CapabilityType.PLATFORM, System.getenv("SELENIUM_PLATFORM"));
+			WebDriver driver = new RemoteWebDriver(
+			            new URL("http://perion1:9cf7a20a-3fb3-4775-bc76-ea671a02aa8a@ondemand.saucelabs.com:80/wd/hub"),
+			                desiredCapabilities);
+	    }
 		
 		@After
 		public void testShutDown(){
