b3b8e2dd7446ea6134e01d5bf65bdfb1645f3333
ronen.yurik <ronen.yurik@RonenYurik.CC.Perion.local>
2015-06-24 15:39:28 +0300

    troubleshooting again add app

--- src/test/java/com/lighthouse/lighthouse/AppManagmentSteps.java
+++ src/test/java/com/lighthouse/lighthouse/AppManagmentSteps.java
@@ -6,7 +6,9 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
@@ -23,7 +25,7 @@ public class AppManagmentSteps extends AbstractPageStepDefinition {
 	
 	WebDriver dr;  
 	
-	@Before("@Application")
+	@Before("@Application1")
 	public void initiateBrowser(){
 		String Turl = System.getenv("QA_URL");
 		
@@ -33,7 +35,7 @@ public class AppManagmentSteps extends AbstractPageStepDefinition {
 		
 	}
 		
-	@After("@Application")
+	@After("@Application1")
 	public void testShutDown(){
 		if (dr != null) {
 			dr.quit();
@@ -48,25 +50,36 @@ public class AppManagmentSteps extends AbstractPageStepDefinition {
 		
 		dr.findElement(By.id("loginBtn")).click();
 		
+		
 		switchFrame("myFrame");
+		//dr.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		
 		dr.findElement(By.id("Email")).sendKeys(username);
+		
 		dr.findElement(By.id("Password")).clear();
 		dr.findElement(By.id("Password")).sendKeys(password);
+		
 		dr.findElement(By.id("login")).click();
-		Thread.sleep(3000);
+		
 	}
 
 	@When("^User select App tab and click on Add app button$")
 	public void select_App_tab_click_addApp() throws Throwable {
 		
 		dr.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
 		dr.findElement(By.cssSelector("[href='#/appsList']")).click();
+		
 		AppListBaseURL = dr.getCurrentUrl();
+		
 		dr.findElement(By.id("addAppBtn")).click();	
+		
 	}
 
 	@When("^Enter App ([^\"]*) upload ([^\"]*) select ([^\"]*) Enter packageID ([^\"]*) choose category ([^\"]*)$")
 	public void create_App(String name, String icon, int platform, String packageID, String category) throws Throwable {
+		
+		//set unique name
 		if (!name.isEmpty()) {
 			appName = name + platform + Time;
 		}
@@ -74,10 +87,15 @@ public class AppManagmentSteps extends AbstractPageStepDefinition {
 			appName = "";
 		}
 		dr.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		
 		dr.findElement(By.id("name")).sendKeys(appName);
+		
 		dr.findElement(By.id("platformType" + platform)).click();
+		
 		dr.findElement(By.id("bundle")).sendKeys(packageID);
+		
 		if (!category.isEmpty()){
+			
 			Select dropdown = new Select (dr.findElement(By.id("appsCategory")));
 			dropdown.selectByVisibleText(category);
 		}
@@ -99,8 +117,10 @@ public class AppManagmentSteps extends AbstractPageStepDefinition {
 		
 		String temp = ".//a[text()='" + appName + "']";
 		By by = By.xpath(temp);
+		
 		WebElement elem = dr.findElement(by);
 		boolean isElementExist = null!=elem?true:false;
+		
 		Assert.assertTrue("New App creation Pass!", isElementExist);
 		
 	}
@@ -114,16 +134,23 @@ public class AppManagmentSteps extends AbstractPageStepDefinition {
 	@Then("^Validate back to app list$")
 	public void Validate_back_to_app_list() throws Throwable {
 		
+		
 		String CurrURL = dr.getCurrentUrl();
+		
+		
 		if (AppListBaseURL .equals(CurrURL)) {
+			
 			boolean cancel = true;
 			Assert.assertTrue(cancel);
 		}
 		else {
 			boolean cancel = false;
 			Assert.assertTrue(cancel);
+			
 		}
+	
 	}
+	
 	
 	public void switchFrame(String frameId) {
 		   dr.switchTo().defaultContent();
