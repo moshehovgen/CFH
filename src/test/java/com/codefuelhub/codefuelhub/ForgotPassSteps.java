79792da5d50668bda815584f04701a1799470898
nofard <nofard@perion.com>
2015-10-15 19:05:32 +0300

    reports - reset password

--- src/test/java/com/codefuelhub/codefuelhub/ForgotPassSteps.java
+++ src/test/java/com/codefuelhub/codefuelhub/ForgotPassSteps.java
@@ -8,6 +8,7 @@ import com.applitools.eyes.Eyes;
 import com.applitools.eyes.RectangleSize;
 import com.applitools.eyes.ProxySettings;
 
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
@@ -16,14 +17,15 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class ForgotPassSteps extends AbstractPageStepDefinition {
-
+	private Scenario scenario;
 	WebDriver dr;
 	MailinatorImplement mailObj = new MailinatorImplement();
 	Eyes eyes;
 	long Time = System.currentTimeMillis();
 	
 	@Before("@Password")
-	public void initiateBrowser(){
+	public void initiateBrowser(Scenario scenario){
+		this.scenario = scenario;
 		eyes = initApplitools(eyes);
 		
 		init();
@@ -59,9 +61,8 @@ public class ForgotPassSteps extends AbstractPageStepDefinition {
 			dr.findElement(By.id("forgot-password")).click();
 		} catch (Exception e) {
 			System.out.println("Unable to click on forgot password button: " + e.getMessage());
+			scenario.write("Unable to click on forgot password button: " + e.getMessage());
 		}
-		
-		
 	}
 	
 	
@@ -72,6 +73,7 @@ public class ForgotPassSteps extends AbstractPageStepDefinition {
 			dr.findElement(By.id("Email")).sendKeys(mailAddress);
 		} catch (Exception e) {
 			System.out.println("Unable to enter email address: " + e.getMessage());
+			scenario.write("Unable to enter email address: " + e.getMessage());
 		}
 	}
 	
@@ -90,6 +92,7 @@ public class ForgotPassSteps extends AbstractPageStepDefinition {
 		} catch(Exception e){
 			takeScreenShot(dr, "reset_pass_fail");
 			System.out.println("Click on reset didn't work: "+ e.getMessage());
+			scenario.write("Click on reset didn't work: "+ e.getMessage());
 			System.out.println("Find screen shot at: " + PS_FILE_NAME + "\\reset_pass... ");
 		}
 	}
@@ -106,6 +109,7 @@ public class ForgotPassSteps extends AbstractPageStepDefinition {
 
 		} catch (Exception e) {
 			System.out.println("Failed clicking on mail: " +e.getMessage());
+			scenario.write("Failed clicking on mail: " +e.getMessage());
 			takeScreenShot(dr, "fail_click_mail");
 			System.out.println("Find screen shot at: " + PS_FILE_NAME + "\\fail_click_mail...");
 		}
@@ -124,6 +128,7 @@ public class ForgotPassSteps extends AbstractPageStepDefinition {
 		
 		} catch (Exception e) {
 			System.out.println("Failed to enter forgot password link: " + e.getMessage());
+			scenario.write("Failed to enter forgot password link: " + e.getMessage());
 		}
 		
 	}
@@ -142,6 +147,7 @@ public class ForgotPassSteps extends AbstractPageStepDefinition {
 			System.out.println("Failed to fill in new password: " + e.getMessage());
 			takeScreenShot(dr, "failed_reset_pass_page");
 			System.out.println("Find screen shot at: " + PS_FILE_NAME + "\\failed_reset_pass_page...");
+			scenario.write("Failed to fill in new password: " + e.getMessage());
 		}
 		
 	}
@@ -168,6 +174,7 @@ public class ForgotPassSteps extends AbstractPageStepDefinition {
 		} catch (Throwable e) {
 			System.out.println("Failed to login with new password: " + e.getMessage());
 			takeScreenShot(dr, "failed_login");
+			scenario.write("Failed to login with new password: " + e.getMessage());
 			System.out.println("Find screen shot at: " + PS_FILE_NAME + "\\failed_login...");
 		}
 	}
